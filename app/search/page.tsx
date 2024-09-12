5f5a6fcd2dd7895232125da738df5a0cbcd6ce44
onam <onamkrverma@gmail.com>
2024-09-12 21:08:06 +0530

    fix: resolve build error

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -16,7 +16,7 @@ const Search = async ({
     <div className="inner-container flex flex-col gap-6 !pb-20">
       <div className="flex flex-col gap-4 my-4">
         <h2 className="capitalize font-bold text-sm sm:text-lg  ">
-          Search results for "{searchQuery}"
+          Search results for {`"${searchQuery}"`}
         </h2>
         {searchResult.data.results.map((song) => (
           <SongsCollection key={song.id} song={song} />
