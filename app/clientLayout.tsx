1cac8faba0ba140eaa788810db009387404a4701
onam <onamkrverma@gmail.com>
2024-09-29 19:04:53 +0530

    feat: add update handling for service worker

--- app/clientLayout.tsx
+++ app/clientLayout.tsx
@@ -21,12 +21,32 @@ const ClientLayout = ({
             "Service Worker registered with scope:",
             registration.scope
           );
+
+          registration.onupdatefound = () => {
+            const installingWorker = registration.installing;
+            if (installingWorker) {
+              installingWorker.onstatechange = () => {
+                if (installingWorker.state === "installed") {
+                  if (navigator.serviceWorker.controller) {
+                    // New update available
+                    if (confirm("New content is available; please refresh.")) {
+                      window.location.reload();
+                    }
+                  } else {
+                    // Content is cached for offline use
+                    console.log("Content is cached for offline use.");
+                  }
+                }
+              };
+            }
+          };
         })
         .catch((error) => {
           console.error("Service Worker registration failed:", error);
         });
     }
   }, []);
+
   return (
     <GlobalContextProvider>
       <main className="container relative">
